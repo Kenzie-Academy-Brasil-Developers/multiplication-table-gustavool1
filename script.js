687a0c36833f68853c7dd584f459e4180c9e84db
gustavo <go8895806@gmail.com>
2021-06-17 16:24:24 -0300

    Finalização

---
+++ script.js
@@ -0,0 +1,17 @@
+function matriz(lines){
+    let matriz = []
+    for(let i=0; i<=lines; i++){
+      matriz[i]=[];
+      for(let c=0; c<=lines; c++){
+        matriz[i][c]= 0
+      }
+    }
+    for(let i= 0; i<=lines;i++){
+      for(let cc =0 ; cc<=lines; cc++){
+        
+        matriz[i][cc] = i*cc
+        
+      }
+    }
+    console.table(matriz)
+   }
